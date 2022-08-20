84d898980bc2bce18cf076a3e0fce3b69a70da7f
mike <mich.rivera84@gmail.com>
2022-08-20 14:45:52 -0400

    I'm going to call it done. There's an issue with the banner at the bottom changing to 100% width but I don't feel like hunting the cause of it.

--- v2/script.js
+++ v2/script.js
@@ -1,6 +1,6 @@
-let targetClass1 = document.querySelector('div') //add tile background2
-let targetClass2 = document.querySelector('body') //add tild background1
-let targetClass3 = document.querySelector('.quote-section') //add tile background2
+let targetClass1 = document.querySelector('div') 
+let targetClass2 = document.querySelector('body') 
+let targetClass3 = document.querySelector('.quote-section') 
 let targetClass4 = document.querySelector('.banner-section .banner')
 let targetClass5 = document.querySelector('.info-section .info-card-container')
 
@@ -15,26 +15,16 @@ img1.src = 'images/1-norm.png'
 img2.src = 'images/2-norm.png'
 img3.src = 'images/3-norm.png'
 img4.src = 'images/4-norm.png'
+targetClass2.classList.remove('anim-background')
 
-
-sobriety = false
+sobriety = true
 
 function colorSwitch() {
     if (sobriety == false) {
         
         sobriety = true
 
-        targetClass1.classList.remove('anim-layer2')
-        targetClass2.classList.remove('anim-layer1')
-        targetClass3.classList.remove('anim-layer2')
-        targetClass4.classList.remove('anim-layer2')
-        targetClass5.classList.remove('anim-layer1')
-
-        imgMain.src = 'images/main-norm.png'
-        img1.src = 'images/1-norm.png'
-        img2.src = 'images/2-norm.png'
-        img3.src = 'images/3-norm.png'
-        img4.src = 'images/4-norm.png'
+        location.reload()
 
     } else {
         
@@ -42,6 +32,7 @@ function colorSwitch() {
 
         targetClass1.classList.add('anim-layer2')
         targetClass2.classList.add('anim-layer1')
+        targetClass2.classList.add('anim-background')
         targetClass3.classList.add('anim-layer2')
         targetClass4.classList.add('anim-layer2')
         targetClass5.classList.add('anim-layer1')
@@ -51,8 +42,27 @@ function colorSwitch() {
         img2.src = 'images/2.gif'
         img3.src = 'images/3.gif'
         img4.src = 'images/4.gif'
+
+        document.getElementById('paranoia1').innerHTML = "Oh god... Oh no....."
+        document.getElementById('paranoia2').innerHTML = "WTF just happened"
+        document.getElementById('paranoia3').innerHTML = "Where am I?"
+        document.getElementById('paranoia4').innerHTML = "Who's there??"
+        document.getElementById('paranoia5').innerHTML = "What the hell did I just sign up for?"
+        document.getElementById('paranoia6').innerHTML = "Eyes... burning. Mind..... flashing colors and unreal memories. A harsh bellowing rings out around me, drowning out the sound of my own thoughts!"
+        document.getElementById('paranoia7').innerHTML = "Get me out of here!!"
+        document.getElementById('paranoia8').innerHTML = "Hot light boiling the inner fluids of my eyes"
+        document.getElementById('paranoia9').innerHTML = "DEATH!"
+        document.getElementById('paranoia10').innerHTML = "VOID!"
+        document.getElementById('paranoia11').innerHTML = "LIFE!"
+        document.getElementById('paranoia12').innerHTML = "VOID!"
+        document.getElementById('paranoia13').innerHTML = "\"We have been to the moon, we have charted the depths of the ocean and the heart of the atom, but we have a fear of looking inward to ourselves because we sense that is where all the contradictions flow together.\""
+        document.getElementById('paranoia14').innerHTML = "- Terrance McKenna"
+        document.getElementById('paranoia15').innerHTML = "Knowledge blossoming at my visual periphery!"
+        document.getElementById('paranoia16').innerHTML = "Reduced to a pile of sensory data and residual slime!!"
+        document.getElementById('paranoia17').innerHTML = "Get me out of here!!"
+        
         
     }
 }
 
-console.log(img2.src, img1.src);
+console.log("Hello there.");
